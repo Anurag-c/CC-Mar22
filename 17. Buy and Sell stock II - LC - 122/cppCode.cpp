358035fb602ce485eadc22b0abdfcfae9da67e0f
Nampally Anurag <anurag.nampally@gmail.com>
2022-03-12 20:38:00 +0530

    Day 3 S2

---
+++ 17. Buy and Sell stock II - LC - 122/cppCode.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& nums)
+    {
+        int n = nums.size();
+        int ans = 0;
+        
+        for(int i = 1; i < n; i++)
+            if(nums[i - 1] < nums[i])
+                ans += nums[i] - nums[i - 1];
+        
+        return ans;
+    }
+};
