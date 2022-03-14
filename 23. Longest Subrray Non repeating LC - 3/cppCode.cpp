b84e4693d5c49125da1e42f06da9cbcb40a7fa68
Nampally Anurag <anurag.nampally@gmail.com>
2022-03-14 13:42:34 +0530

    day 4

--- 23. Longest Subrray Non repeating LC - 3/cppCode.cpp
+++ 23. Longest Subrray Non repeating LC - 3/cppCode.cpp
@@ -19,3 +19,29 @@ public:
         return res;
     }
 };
+
+int lengthOfLongestSubstring(string s) 
+{
+    int n = s.length();
+    int start = 0;
+    int end = 0;
+    int repeat = 0;
+    int ans = 0;
+    vector<int>hm(256, 0);
+    
+    while(end < n)
+    {
+        if(++hm[s[end++]] == 2) 
+            repeat++;
+        
+        while(repeat > 0)
+        {
+            if(--hm[s[start++]] == 1)
+                repeat--;
+        }
+        
+        ans = max(ans, end - start);
+    }
+    
+    return ans;
+}
